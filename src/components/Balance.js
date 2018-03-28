a2c1faae88472fd90271a1b18a00a63cb1979fd3
Michael <michael.bretagne@gmail.com>
2018-03-28 14:54:28 -0700

    Add Token Balance component

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,45 +1,69 @@
 import React, { Component } from "react";
-import { Button, Message, Input } from "semantic-ui-react";
+import { Button, Grid, Icon } from "semantic-ui-react";
 import web3 from "../ethereum/web3";
 import ContractToken from "../ethereum/factory";
+import styles from "./Styles.css";
 
 class BalanceOf extends Component {
   state = {
     balance: "",
-    address: "",
-    metaMaskFound: true,
-    messageError1: "",
-    messageError2: ""
+    usd: ""
   };
 
   async componentWillMount() {
     const accounts = await web3.eth.getAccounts();
-    if (accounts[0] === undefined) {
-      this.setState({
-        messageError1: "MetaMask not found! Make sure",
-        messageError2: "is installed and you are logged in!",
-        metaMaskFound: false
-      });
-    }
-
     const balance = await ContractToken.methods.balanceOf(accounts[0]).call();
-    this.setState({ address: accounts[0], balance: balance });
+    const usd = Math.round(balance / 10000000).toFixed(2);
+    this.setState({ balance, usd });
   }
 
   render() {
     return (
-      <div style={{ marginTop: 20 }}>
-        {!this.state.metaMaskFound && (
-          <Message error>
-            <p>
-              {this.state.messageError1}{" "}
-              <a href="https://metamask.io/">MetaMask</a>{" "}
-              {this.state.messageError2}
-            </p>
-          </Message>
-        )}
-        <h3>BALANCE</h3>
-        <h1>{this.state.balance} BZH</h1>
+      <div className={styles.balanceComponent}>
+        <Grid columns="equal" textAlign="left" padded>
+          <Grid.Row>
+            <Grid.Column>
+              <Icon name="circle" size="small" color="teal" />MY ACCOUNT
+            </Grid.Column>
+            <Grid.Column />
+            <Grid.Column />
+            <Grid.Column />
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column>
+              <div className={styles.balance}>BALANCE</div>
+              <div className={styles.balanceValue}>
+                BZH{" "}
+                <span style={{ fontWeight: "bold" }}>{this.state.balance}</span>
+              </div>
+            </Grid.Column>
+            <Grid.Column>
+              <div className={styles.balance}>
+                <span style={{ color: "#9F9F9F" }}>COUNTERVALUE</span>
+              </div>
+              <div className={styles.balanceValue}>
+                <span style={{ color: "#9F9F9F" }}>USD {this.state.usd}</span>
+              </div>
+            </Grid.Column>
+            <Grid.Column />
+            <Grid.Column>
+              <div className={styles.rightAlign}>
+                <div className={styles.actions}>ACTIONS</div>
+                <div className={styles.sendButton}>
+                  <Button color="teal" circular>
+                    SEND
+                  </Button>
+                </div>
+                <div className={styles.receiveButton}>
+                  <Button color="teal" circular>
+                    RECEIVE
+                  </Button>
+                </div>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
