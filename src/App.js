fb4254b32f4fe48c8cc87085a11394838df6d0ee
Michael <michael.bretagne@gmail.com>
2018-03-29 10:00:05 -0700

    Add metamask error handler

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Container, Grid, Message } from "semantic-ui-react";
 import BalanceOf from "./components/Balance";
 import Sending from "./components/Sending";
 import Receiving from "./components/Receiving";
 import Transactions from "./components/Transactions";
-import { Container, Grid } from "semantic-ui-react";
+import Header from "./components/Header";
+import web3 from "./ethereum/web3";
 
 class App extends Component {
+  state = {
+    metaMaskFound: true,
+    messageError1: "",
+    messageError2: ""
+  };
+
+  async componentWillMount() {
+    const accounts = await web3.eth.getAccounts();
+    if (accounts[0] === undefined) {
+      this.setState({
+        messageError1: "MetaMask not found! Make sure",
+        messageError2: "is installed and you are logged in!",
+        metaMaskFound: false
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Breizh Coin Token Wallet</h1>
-          <h3 className="App-title">
-            The easiest way to transfer your tokens!
-          </h3>
-        </header>
         <Container>
+          <Header />
+          <div style={{ marginTop: 20 }}>
+            {!this.state.metaMaskFound && (
+              <Message error>
+                <p>
+                  {this.state.messageError1}{" "}
+                  <a href="https://metamask.io/">MetaMask</a>{" "}
+                  {this.state.messageError2}
+                </p>
+              </Message>
+            )}
+          </div>
           <BalanceOf />
-
-          <Grid divided="vertically">
-            <Grid.Row columns={2}>
-              <Grid.Column>
-                <Sending />
-              </Grid.Column>
-              <Grid.Column>
-                <Receiving />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
           <Transactions />
+          <Sending />
         </Container>
       </div>
     );
