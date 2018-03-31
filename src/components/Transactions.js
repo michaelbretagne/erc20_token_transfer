bf423191e7da30fea5a40db8dc0d8ebd3c7e7ca8
Michael <michael.bretagne@gmail.com>
2018-03-30 21:42:27 -0700

    Fetch additional data from Etherscan API

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -6,70 +6,77 @@ import { etherscanApiKey } from "../api_keys/keys";
 var moment = require("moment");
 
 class Transactions extends Component {
+  state = {
+    data: []
+  };
+
   async componentWillMount() {
+    // User accounts
     const accounts = await web3.eth.getAccounts();
-    const contractAddress = "	0x405be360e813b293b1b162ab10aba9b2813917d6";
+    // Token contract address
+    const contractAddress = "0x405be360e813b293b1b162ab10aba9b2813917d6";
+    // Keccak256 hash of the signature of the event.
     const topic =
       "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
-    const apiUrl = await `https://api-rinkeby.etherscan.io/api?module=logs&action=getLogs&fromBlock=2013369&toBlock=latest&address=${contractAddress}&topic0=${topic}&apikey=${etherscanApiKey}`;
+    const urlEventLog = await `https://api-rinkeby.etherscan.io/api?module=logs&action=getLogs&fromBlock=2013369&toBlock=latest&address=${contractAddress}&topic0=${topic}&apikey=${etherscanApiKey}`;
 
-    // Fetch data from Etherscan
-    const response = await fetch(apiUrl);
+    // Fetch event log to retrieve transactions data of the contract
+    const contractLog = await fetch(urlEventLog);
 
-    if (response.status >= 400) {
+    // Check status of API call
+    if (contractLog.status >= 400) {
       throw new Error("Bad response from server");
     }
-    const data = await response.json();
+    // JSON format of the data
+    const contractData = await contractLog.json();
 
-    for (let result of data.result) {
-      // Get address of operation
-      const hexString = result.data;
-      const value = parseInt(hexString, 16) / 100;
-      // Get date of operation
-      const timeStamp = result.timeStamp;
-      const convertedTime = new Date(timeStamp * 1000);
-      const formatedTime = moment(convertedTime).format("YYYY-MM-DD HH:mm");
-      // Get value of operation
-      const txReceipt = await web3.eth.getTransactionReceipt(
-        result.transactionHash
-      );
+    // Iterate through transactions data
+    for (let result of contractData.result) {
+      // Check for the token address
+      if (result.address === contractAddress) {
+        // Topic 1 = sender, Topic 2 = receiver
+        const from = result.topics[1].replace("000000000000000000000000", "");
+        const to = result.topics[2].replace("000000000000000000000000", "");
+        // Get hash of each transaction
+        const hash = result.transactionHash;
 
-      const hashUrl = `https://rinkeby.etherscan.io/tx/${
-        txReceipt.transactionHash
-      }`;
+        // Check if the user sent or received trnascations
+        if (
+          parseInt(accounts[0], 16) === parseInt(to, 16) ||
+          parseInt(accounts[0], 16) === parseInt(from, 16)
+        ) {
+          // Assign data variables
+          const hexString = result.data;
+          const hashUrl = `https://rinkeby.etherscan.io/tx/${hash}`;
+          const timeStamp = new Date(result.timeStamp * 1000);
+          const formatedTime = moment(timeStamp).format("MM-DD-YYYY HH:mm");
+          let value = parseInt(hexString, 16) / 100;
 
-      if (parseInt(accounts[0]) === parseInt(txReceipt.from)) {
-        this.setState({
-          value: [
-            ...this.state.value,
-            { amount: "-" + value.toFixed(2), color: "red" }
-          ]
-        });
-      } else {
-        this.setState({
-          value: [
-            ...this.state.value,
-            { amount: "+" + value.toFixed(2), color: "green" }
-          ]
-        });
+          // Check if user sent or received the transaction then assign operator and style (color)
+          if (parseInt(accounts[0], 16) === parseInt(from, 16)) {
+            value = { amount: "-" + value.toFixed(2), color: "red" };
+          } else {
+            value = { amount: "+" + value.toFixed(2), color: "green" };
+          }
+
+          // Set state
+          this.setState({
+            data: [
+              ...this.state.data,
+              {
+                txHash: hash,
+                link: hashUrl,
+                time: formatedTime,
+                value,
+                from,
+                to
+              }
+            ]
+          });
+        }
       }
-      // const hashArr = [txReceipt.transactionHash, hashUrl];
-      // Append data to state
-      this.setState({
-        txHash: [
-          ...this.state.txHash,
-          { txId: txReceipt.transactionHash, link: hashUrl }
-        ],
-        date: [...this.state.date, formatedTime]
-      });
     }
   }
-
-  state = {
-    date: [],
-    value: [],
-    txHash: []
-  };
 
   render() {
     return (
@@ -97,60 +104,39 @@ class Transactions extends Component {
             </Grid.Column>
           </Grid.Row>
 
-          <Grid.Row>
-            <Grid.Column width={4} className={styles.leftAlign}>
-              {this.state.date.map(function(time) {
-                return (
-                  <div className={styles.operationRow}>
-                    <li className={styles.transactionListTime} key={time}>
-                      {time}
+          {/* Iterate through the data */}
+          {this.state.data.map((data, index) => {
+            return (
+              <Grid.Row key={index}>
+                {/* Times and dates column */}
+                <Grid.Column width={4} className={styles.leftAlign}>
+                  <div className={styles.operationRow} key={index}>
+                    <li className={styles.transactionListTime}>{data.time}</li>
+                  </div>
+                </Grid.Column>
+                {/* Transaction hashes and links to etherscan*/}
+                <Grid.Column width={8} className={styles.leftAlign}>
+                  <div className={styles.operationRow} key={index}>
+                    <li className={styles.transactionList}>
+                      <a href={data.link}>{data.txHash}</a>
                     </li>
                   </div>
-                );
-              })}
-            </Grid.Column>
-
-            <Grid.Column width={8} className={styles.leftAlign}>
-              {this.state.txHash.map(function(txHash) {
-                return (
-                  <div className={styles.operationRow}>
-                    <li className={styles.transactionList} key={txHash}>
-                      <a href={txHash.link}>{txHash.txId}</a>
-                    </li>
-                  </div>
-                );
-              })}
-            </Grid.Column>
-
-            <Grid.Column width={4} className={styles.rightAlign}>
-              {this.state.value.map(function(value) {
-                return (
+                </Grid.Column>
+                {/* Amount transfered */}
+                <Grid.Column width={4} className={styles.rightAlign}>
                   <div className={styles.operationRow}>
                     <li
                       className={styles.transactionList}
-                      style={{ color: value.color }}
-                      key={value}
+                      style={{ color: data.value.color }}
                     >
-                      BZH {value.amount}
+                      BZH {data.value.amount}
                     </li>
                   </div>
-                );
-              })}
-            </Grid.Column>
-          </Grid.Row>
+                </Grid.Column>
+              </Grid.Row>
+            );
+          })}
         </Grid>
-
-        {/* <div className={styles.action}>
-          <h3 className={styles.leftAlign}>LAST OPERATIONS</h3>
-        </div>
-        <ul className="transcationsList">
-          {this.state.date.map(function(time) {
-            return <li key={time}>{time}</li>;
-          })}
-          {this.state.date.map(function(time) {
-            return <li key={time}>{time}</li>;
-          })}
-        </ul> */}
       </div>
     );
   }
