9e48f84be82bf41299eb2f226742eda4a7fc8269
Michael <michael.bretagne@gmail.com>
2018-04-02 15:54:22 -0700

    Add tests assertions

--- test/ERC20_token.test.js
+++ test/ERC20_token.test.js
@@ -13,13 +13,13 @@ beforeEach(async () => {
   // Get account that will be use to create the contract
   accounts = await web3.eth.getAccounts();
 
-  // Deploy contract
+  // Deploy contract. Arguments(initSupply, maxSupply, freeTokensPerAddress)
   tokenContract = await new web3.eth.Contract(
     JSON.parse(compiledContract.interface)
   )
-    .deploy({ data: compiledContract.bytecode, arguments: [10000000] })
+    .deploy({ data: compiledContract.bytecode, arguments: [1000, 1199, 100] })
     .send({ from: accounts[0], gas: "1000000" });
-
+  // Get balance
   balance = await tokenContract.methods.balanceOf(accounts[0]).call();
   balance = parseInt(balance);
 });
@@ -30,17 +30,24 @@ describe("ERC20 token contract", () => {
   });
 
   it("it return the correct balance", async () => {
-    assert.equal(balance, 10000000);
+    assert.equal(balance, 1000);
+  });
+
+  it("marks caller as the contract manager", async () => {
+    const manager = await tokenContract.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+
+  it("check the maxSupply", async () => {
+    const maxSupply = await tokenContract.methods.maximumSupply().call();
+    assert.equal(maxSupply, 1199);
   });
 
   it("it get free tokens", async () => {
-    await tokenContract.methods.getFreeToken().send({
-      from: accounts[0],
-      gas: "1000000"
-    });
+    await tokenContract.methods.getFreeToken().send({ from: accounts[0] });
 
     let newBalance = await tokenContract.methods.balanceOf(accounts[0]).call();
     newBalance = parseInt(newBalance);
-    assert.equal(balance + 1000, newBalance);
+    assert.equal(newBalance, 1100);
   });
 });
